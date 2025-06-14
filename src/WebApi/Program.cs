bf6998672ab798bc61aa59ce7d46c23655091291
staffing-rahul-srivastava <V-Rahul.Srivastava@bain.com>
2025-06-14 07:32:35 +0530

    Trigger CI/CD (#16)

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,13 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,7 +15,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing!", "Bracing!", "Chilly!", "Cool!", "Mild!", "Warm!", "Balmy!", "Hot!", "Sweltering!", "Scorching!!"
+    "Freezing!", "Bracing!", "Chilly!", "Cool!", "Mild!", "Warm!", "Balmy!", "Hot!", "Sweltering!", "Scorching!"
 };
 
 app.MapGet("/weatherforecast", () =>
