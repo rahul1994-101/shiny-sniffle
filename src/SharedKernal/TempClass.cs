fbd5a2627cd51c0fa9e6d2622fe136023eab2039
staffing-rahul-srivastava <V-Rahul.Srivastava@bain.com>
2025-06-14 07:17:29 +0530

    Trigger CI/CD (#14)

--- src/SharedKernal/TempClass.cs
+++ src/SharedKernal/TempClass.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SharedKernal
 {
     internal class TempClass
     {
-        public int MyProperty { get; set; }
     }
 }
