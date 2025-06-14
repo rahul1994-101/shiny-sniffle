a160b893c2145916ff18378575b9b2810a7550a5
Rahul Srivastava <V-Rahul.Srivastava@bain.com>
2025-06-14 07:22:14 +0530

    Trigger CI/CD

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -18,7 +18,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing!", "Bracing!", "Chilly!", "Cool!", "Mild!", "Warm!", "Balmy!", "Hot!", "Sweltering!", "_Scorching!!"
+    "Freezing!", "Bracing!", "Chilly!", "Cool!", "Mild!", "Warm!", "Balmy!", "Hot!", "Sweltering!", "Scorching!!"
 };
 
 app.MapGet("/weatherforecast", () =>
