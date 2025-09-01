14019dee656e0909f7f870f2ddb772023d655be1
staffing-rahul-srivastava <V-Rahul.Srivastava@bain.com>
2025-09-01 16:27:21 +0530

    .. (#31)

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -15,7 +15,7 @@ app.UseHttpsRedirection();
 
 var summaries = new[]
 {
-    "Freezing!", "Bracing!", "Chilly!", "Cool!", "Mild!", "Warm!", "Balmy!", "workflow_dispatch_04"
+    "Freezing!", "Bracing!", "Chilly!", "Cool!", "Mild!", "Warm!", "Balmy!", "workflow_dispatch_05"
 };
 
 app.MapGet("/weatherforecast", () =>
