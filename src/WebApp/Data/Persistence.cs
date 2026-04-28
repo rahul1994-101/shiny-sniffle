53785b7d431db8386e345ef2c4f74d06528fded4
Rahul Srivastava <rahul1994.101@gmail.com>
2026-04-28 08:22:05 +0530

    ..

--- src/WebApp/Data/Persistence.cs
+++ src/WebApp/Data/Persistence.cs
@@ -9,6 +9,13 @@ public sealed class Persistence(AppDbContext _ctx)
 {
     public async Task<SignInResponse?> SignInAsync(SignInRequest signInRequest)
     {
+        return new SignInResponse
+        {
+            Id = Guid.NewGuid(),
+            Email = signInRequest.EmailId,
+            FullName = "John Doe"
+        };
+
         //var encPassword = signInRequest.Password.Encrypt();
         var encPassword = signInRequest.Password;
 
