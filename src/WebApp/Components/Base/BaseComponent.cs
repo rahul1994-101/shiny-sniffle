271aa9a491cdb3f98ef2b9e3a9ce4f3bdfc185b1
Rahul Srivastava <V-Rahul.Srivastava@bain.com>
2026-05-20 08:51:44 +0530

    ..

---
+++ src/WebApp/Components/Base/BaseComponent.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using WebApp.Data;
+
+namespace WebApp.Components.Base;
+
+/// <summary>
+/// Base class for interactive Blazor components. Provides shared injections and layout helpers.
+/// Layout shells (e.g. MainLayout) should inherit <see cref="LayoutComponentBase"/> instead.
+/// </summary>
+public abstract class BaseComponent : ComponentBase
+{
+    #region # Init
+
+    [Inject]
+    protected Features Features { get; set; } = null!;
+
+    [Inject]
+    protected NavigationManager NavigationManager { get; set; } = null!;
+
+    [Inject]
+    protected IJSRuntime JsRuntime { get; set; } = null!;
+
+    [CascadingParameter(Name = "NotifyLayoutRefresh")]
+    public EventCallback NotifyLayoutRefresh { get; set; }
+
+    #endregion
+
+    #region # Helpers
+
+    protected Task RefreshLayoutAsync() =>
+        NotifyLayoutRefresh.HasDelegate
+            ? NotifyLayoutRefresh.InvokeAsync()
+            : Task.CompletedTask;
+
+    #endregion
+}
