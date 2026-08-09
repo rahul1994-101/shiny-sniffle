df581f9b699badd6b95cdbf40b821dbff94a8d4d
Rahul Srivastava <V-Rahul.Srivastava@bain.com>
2026-08-09 12:48:53 +0530

    ..

--- src/Infrastructure/GlobalUsings.cs
+++ src/Infrastructure/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using Infrastructure.Persistence.Entities;
+global using Infrastructure.Persistence.Entities.dbo;
+global using Infrastructure.Persistence.Entities.workspace;
 global using Infrastructure.Foundry;
 global using Infrastructure.Mailbox;
